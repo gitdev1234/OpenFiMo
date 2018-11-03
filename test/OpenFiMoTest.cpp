825471f14ea61abbb30983a35811b820b51b62b7
gitdev1234 <gitdev@web.de>
2018-11-03 17:07:28 +0100

    refactored test case

--- test/OpenFiMoTest.cpp
+++ test/OpenFiMoTest.cpp
@@ -14,20 +14,51 @@
 
 using namespace  std;
 
-TEST_CASE("Test TODO") {
+TEST_CASE("Test adding amount owners, transactions and calculation of transaction") {
+
     OpenFinanceMonitor ofm;
     ofm.addMoneyGiver("ABC GmbH",0);
-    ofm.addMoneyGiver("cdf AG",0);
-    ofm.addYou("anon",0);
-    ofm.addMoneyReceiver("insurance abc",0);
-    ofm.addMoneyReceiver("rent apartment",0);
+    ofm.addMoneyGiver("CDF AG",0);
+    ofm.addYou("Me",0);
+    ofm.addMoneyReceiver("Insurance 1",0);
+    ofm.addMoneyReceiver("Apartment owner",0);
     ofm.addPiggyBank("Emergency Fund",0);
 
-    ofm.addIncomingTransaction("paycheck",ofm.getAmoutOwnerByName("ABC GmbH"),1000);
-    ofm.addIncomingTransaction("monthly payment",ofm.getAmoutOwnerByName("cdf AG"),200);
-    ofm.addOutgoingExpenseTransaction("insurance payment",ofm.getAmoutOwnerByName("insurance abc"),150);
-    ofm.addOutgoingExpenseTransaction("aparment rent",ofm.getAmoutOwnerByName("rent apartment"),300);
-    ofm.addOutgoingSavingsTransaction("emergency savings",ofm.getAmoutOwnerByName("Emergency Fund"),15);
+    REQUIRE(ofm.getAmoutOwnerByName("ABC GmbH"       ).getName() == "ABC GmbH");
+    REQUIRE(ofm.getAmoutOwnerByName("CDF AG"         ).getName() == "CDF AG");
+    REQUIRE(ofm.getAmoutOwnerByName("Me"             ).getName() == "Me");
+    REQUIRE(ofm.getAmoutOwnerByName("Insurance 1"    ).getName() == "Insurance 1");
+    REQUIRE(ofm.getAmoutOwnerByName("Apartment owner").getName() == "Apartment owner");
+    REQUIRE(ofm.getAmoutOwnerByName("Emergency Fund" ).getName() == "Emergency Fund");
+
+    REQUIRE(ofm.getAmoutOwnerByName("ABC GmbH"       ).getBalance() == 0);
+    REQUIRE(ofm.getAmoutOwnerByName("CDF AG"         ).getBalance() == 0);
+    REQUIRE(ofm.getAmoutOwnerByName("Me"             ).getBalance() == 0);
+    REQUIRE(ofm.getAmoutOwnerByName("Insurance 1"    ).getBalance() == 0);
+    REQUIRE(ofm.getAmoutOwnerByName("Apartment owner").getBalance() == 0);
+    REQUIRE(ofm.getAmoutOwnerByName("Emergency Fund" ).getBalance() == 0);
+
+    ofm.addIncomingTransaction("Paycheck",ofm.getAmoutOwnerByName("ABC GmbH"),1000);
+    ofm.addIncomingTransaction("Monthly payment",ofm.getAmoutOwnerByName("CDF AG"),200);
+    ofm.addOutgoingExpenseTransaction("Insurance payment",ofm.getAmoutOwnerByName("Insurance 1"),150);
+    ofm.addOutgoingExpenseTransaction("Aparment rent",ofm.getAmoutOwnerByName("Apartment owner"),300);
+    ofm.addOutgoingSavingsTransaction("Emergency savings",ofm.getAmoutOwnerByName("Emergency Fund"),15);
 
     ofm.calculateCurrentTransactions();
+
+    REQUIRE(ofm.getAmoutOwnerByName("ABC GmbH"       ).getBalance() == -1000);
+    REQUIRE(ofm.getAmoutOwnerByName("CDF AG"         ).getBalance() == -200);
+    REQUIRE(ofm.getAmoutOwnerByName("Me"             ).getBalance() == +735);
+    REQUIRE(ofm.getAmoutOwnerByName("Insurance 1"    ).getBalance() == +150);
+    REQUIRE(ofm.getAmoutOwnerByName("Apartment owner").getBalance() == +300);
+    REQUIRE(ofm.getAmoutOwnerByName("Emergency Fund" ).getBalance() ==  +15);
+
+    ofm.calculateCurrentTransactions();
+
+    REQUIRE(ofm.getAmoutOwnerByName("ABC GmbH"       ).getBalance() == -2000);
+    REQUIRE(ofm.getAmoutOwnerByName("CDF AG"         ).getBalance() == -400);
+    REQUIRE(ofm.getAmoutOwnerByName("Me"             ).getBalance() == +1470);
+    REQUIRE(ofm.getAmoutOwnerByName("Insurance 1"    ).getBalance() == +300);
+    REQUIRE(ofm.getAmoutOwnerByName("Apartment owner").getBalance() == +600);
+    REQUIRE(ofm.getAmoutOwnerByName("Emergency Fund" ).getBalance() ==  +30);
 }
